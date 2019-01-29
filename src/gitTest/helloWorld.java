1df4695cd73a30e887ccdf5c3cf99f4bca89a4d8
haolunwu <haolunwu@mac-air-26.cs.iastate.edu>
2019-01-28 18:11:40 -0600

    latest change

--- src/gitTest/helloWorld.java
+++ src/gitTest/helloWorld.java
@@ -2,7 +2,7 @@ package gitTest;
 
 public class helloWorld {
 		public static void main(String[] args) {
-			System.out.println("Hello World!");
+			System.out.println("Hello Grandpa!");
 			
 			
 		}
