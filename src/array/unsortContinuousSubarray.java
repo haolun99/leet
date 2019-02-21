e2417b741e7f10c19098c70a7ea14f71d678336f
haolunwu <haolunwu@mac-air-26.cs.iastate.edu>
2019-02-21 17:44:23 -0600

    add stack question 844

--- src/array/unsortContinuousSubarray.java
+++ src/array/unsortContinuousSubarray.java
@@ -1,13 +1,44 @@
 package array;
 
+import java.util.Arrays;
+
 public class unsortContinuousSubarray {
-	 public int findUnsortedSubarray(int[] nums) {
-	     /* (1) Sort the array and compare it to original array.
-		  (2) Use two for-loop to compare, first loop check from left and find 
-		  first different one and obtain index, second loop check from right and 
-		  get index for first different position.
-		  (3) The length is the difference of two index.
-		  */
-		 return 0;
-	    }
+	public int findUnsortedSubarray(int[] nums) {
+		/*
+		 * (1) Sort the array and compare it to original array. (2) Use two for-loop to
+		 * compare, first loop check from left and find first different one and obtain
+		 * index, second loop check from right and get index for first different
+		 * position. (3) The length is the difference of two index.
+		 */
+		int origin[] = new int[nums.length];
+		int pos1 = 0;
+		int pos2 = 0;
+		int length = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			origin[i] = nums[i];
+		}
+
+		Arrays.parallelSort(nums);
+
+		for (int i = 0; i < origin.length; i++) { // what this for-loop did
+			for (int j = 0; j < nums.length; j++) {
+				if (origin[i] != nums[j]) {
+					pos1 = origin[i];
+				}
+			}
+		}
+
+		for (int j = nums.length - 1; j > 0; j--) {
+			for (int i = origin.length - 1; i > 0; i--) {
+				if (nums[i] != origin[j]) {
+					pos2 = nums[i];
+				}
+			}
+		}
+
+		length = pos2 - pos1;
+
+		return length;
+	}
 }
---
+++ src/stack/backspace.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.util.Stack;
+
+// 844
+public class backspace {
+	
+	// To do: Use index instead stack to improve efficient
+	
+		public boolean backspaceCompare(String S, String T) {
+			
+			return helper(S).equals(helper(T)); // Compare two string directly
+    }
+		
+		// Create helper method outside the given method
+		public String helper(String s) {
+			Stack<Character>stack = new Stack(); // <Character>
+			
+			for(Character c : s.toCharArray()) {
+				if(c != '#') {  // Character '' ; String ""
+					stack.push(c);
+				}else if(!stack.empty()){ 
+											// Check empty in case "a##c"
+										// ? c == '#' && !stack.empty()
+					                      // If else if contain another condition?
+					stack.pop();
+				}
+			}
+			
+			return String.valueOf(stack); // change stack to comparable string
+		}
+}
