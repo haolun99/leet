fe7e1558478dec9e8e542dfdf9232ec286b02633
haolunwu <haolunwu@mac-air-26.cs.iastate.edu>
2019-03-01 17:12:39 -0600

    add 167 twoSumTwo

--- src/array/twoSumTwo.java
+++ src/array/twoSumTwo.java
@@ -6,32 +6,43 @@ import java.util.Map;
 
 public class twoSumTwo {
 	public static void main(String[] args) {
-		int num[] = {0,1,8,7,0,1,6};
-		Arrays.sort(num);
-		for(int i =0 ; i < num.length; i++) {
-		System.out.println(num[i]);
-		}
+		int num[] = {2,7,11,15};
+		int numbers = 9;
+		System.out.println(Arrays.toString(new twoSumTwo().twoSum(num, numbers)));
 	}
-	// To do
-	// Wrong: [2, 7, 11, 15] 9
-	// Return 0,1 rather than 1,2
+
+	// Solution
+	public int[] twoSumOne(int[] nums, int target) {
+        int i=0, j=nums.length-1;
+        while (i<j) {
+            if (nums[i]+nums[j]==target) return new int[]{i+1, j+1}; 
+            // i + 1, j + 1This question treat index start from 1, not 0
+            else if (nums[i]+nums[j]<target) i++;
+            else j--;
+        }
+        return null;
+    }
+	
+	
+	// My answer
 	public int[] twoSum(int[] numbers, int target) {
 		int[] temp = new int[2];
-		int index1 = 0;
-		int index2 = 0;
 	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	    for (int i = 0; i < numbers.length; i++) {
 	        if (map.containsKey(target - numbers[i])) {   // How put array elements into hashMap
 	            temp[1] = i;    
 	            temp[0] = map.get(target - numbers[i]);
-	            return temp;
+	            // return temp; 
+	            // Notice this will terminate program directly without run following 
 	        }
 	        map.put(numbers[i], i);
 	    }
 	    
-	    index1 = Arrays.asList(numbers).indexOf(temp[0]);
-	    index2 = Arrays.asList(numbers).indexOf(temp[1]);
-	    
-	    return new int[]{index1+1, index2+1};
+	    int[] result = new int[2];
+	    for(int i = 0; i < temp.length; i++){
+             result[i] = temp[i] + 1;
+        }
+        
+        return result;
     }
 }
