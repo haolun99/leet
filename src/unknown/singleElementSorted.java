fd5391b7bbf34ea82b3cab270ce51b3800f579a9
haolunwu <haolunwu@mac-air-26.cs.iastate.edu>
2019-03-06 23:57:46 -0600

    add 540 singleElement

--- src/unknown/singleElementSorted.java
+++ src/unknown/singleElementSorted.java
@@ -1,8 +1,19 @@
 package unknown;
+
+import java.util.Arrays;
+
 // 540
 public class singleElementSorted {
-		public int singleNonDuplicate(int[] nums) {
-        
-			return 0;
+		
+	// Correct but slow
+	public int singleNonDuplicate(int[] nums) {
+            Arrays.parallelSort(nums);
+            
+            int result = 0;
+            for(int i = 0; i < nums.length; i++) {
+            	result ^= nums[i];
+            }
+			
+			return result;
     }
 }
