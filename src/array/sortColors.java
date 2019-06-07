b12b58d89cbd5d7556ebcfdd2d002243465c2dd6
Frank Wu <1204926758@qq.com>
2019-06-07 19:03:22 -0400

    add 75 sort colors

---
+++ src/array/sortColors.java
@@ -0,0 +1,34 @@
+package array;
+// 75
+public class sortColors {
+
+	public void sortColors(int[] nums) {
+		int zero = 0;
+		int one = 0;
+		int two = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				zero++;
+			} else if (nums[i] == 1) {
+				one++;
+			} else {
+				two++;
+			}
+		}
+
+		for (int i = 0; i < zero; i++) {
+			nums[i] = 0;
+		}
+
+		for (int i = 0; i < one; i++) {
+			nums[zero + i] = 1;
+		}
+
+		for (int i = 0; i < two; i++) {
+			nums[zero + one + i] = 2;
+		}
+
+	}
+
+}
