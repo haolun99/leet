e751df6a9d3e50974d218ca6949ceaff60a4b560
Frank Wu <1204926758@qq.com>
2019-06-07 17:53:00 -0400

    add 240 search matrix Two

---
+++ src/matrix/searchTwo.java
@@ -0,0 +1,40 @@
+package matrix;
+// 240
+public class searchTwo {
+
+	/*
+	 *     col
+	 * row [1,   4,  7, 11, 15],
+           [2,   5,  8, 12, 19],
+           [3,   6,  9, 16, 22],
+           [10, 13, 14, 17, 24],
+           [18, 21, 23, 26, 30]
+	 */
+	
+	public boolean searchMatrix(int[][] matrix, int target) {
+		// Base case for matrix
+		if(matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+			return false;
+		}
+		
+		int row = 0;
+		int col = matrix[0].length - 1;
+		
+		// Notice row ---> matrix.length
+		//        column ---> matrix[0].length
+		//while(col >= 0 && row <= matrix[0].length - 1) {
+		
+		while(col >= 0 && row <= matrix.length - 1) {
+			   if(target == matrix[row][col]) {
+				return true;
+			}
+			   else if(target > matrix[row][col]) {
+				row++;
+			}
+			   else if(target < matrix[row][col]) {
+				col--;
+			}
+		}
+		return false;
+	}
+}
