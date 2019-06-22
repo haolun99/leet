c513dfebf2340579973e767b52ce40d3a9d37e52
Frank Wu <1204926758@qq.com>
2019-06-22 00:10:40 -0400

    add 387 firstUniqueCharacter

---
+++ src/stack/validParentheses.java
@@ -0,0 +1,48 @@
+package stack;
+// 20
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+// £¨1£© Open brackets must be closed by the same type of brackets.
+// £¨2£© Open brackets must be closed in the correct order.
+// Notice the second is important!
+import java.util.Stack;
+
+public class validParentheses {
+	
+	public boolean isValid(String s) {
+        
+		return false;
+    }
+	
+	
+	
+	// Not work for case "(}" five == six == 0, return true but should retrun false
+//	public boolean isValidWrong(String s) {
+//		Stack<Character> stack = new Stack();
+//		boolean flag = false;
+//		for (char c : s.toCharArray()) {
+//			int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0; 
+//			if (c == '(') {
+//				one++;
+//			} else if(c == ')') {
+//				two++;
+//			}
+//			else if(c == '{') {
+//				three++;
+//			} else if (c == '}') {
+//				four++;
+//			}
+//			else if (c == '[') {
+//				five++;
+//			} else {
+//				six++;
+//			}
+//			
+//			flag = ( one == two) || (three == four) || (five == six);
+//		}
+//		
+//		return flag;
+//	}
+}
