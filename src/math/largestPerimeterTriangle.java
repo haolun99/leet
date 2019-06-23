2ff406711f4fe43d49b19cb9c28e58702b626d9d
Frank Wu <1204926758@qq.com>
2019-06-23 19:11:19 -0400

    add 976 Largest Perimeter Triangle

---
+++ src/math/largestPerimeterTriangle.java
@@ -0,0 +1,17 @@
+package math;
+// 976
+import java.util.Arrays;
+
+public class largestPerimeterTriangle {
+		public int largestPerimeter(int[] A) {
+			Arrays.sort(A);
+            
+            for(int i = A.length - 3; i >= 0; i--) {
+            	if(A[i] + A[i + 1] > A[i + 2]) {
+            		return A[i] + A[i + 1] + A[i + 2];
+            	}
+            }
+			
+			return 0;
+    }
+}
