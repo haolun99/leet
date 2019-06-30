52c3caf91ec3baa3d475230f4e95954f87c6f8cb
Frank Wu <1204926758@qq.com>
2019-06-30 18:33:43 -0400

    add 64 minimumPathSum

---
+++ src/dynamic/minimumPathSum.java
@@ -0,0 +1,26 @@
+package dynamic;
+// 64
+public class minimumPathSum {
+	public int minPathSum(int[][] grid) {
+		int m = grid.length, n = grid[0].length;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == 0 && j == 0)
+					continue;
+				//else if (i == 0) {
+					if(i == 0) {
+					grid[i][j] += grid[i][j - 1];
+				} else if (j == 0) {
+					grid[i][j] += grid[i - 1][j];
+				} else {
+					// grid[i][j] += grid[i - 1][j - 1]; // Wrong
+					grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+				}
+			}
+		}
+
+		return grid[m - 1][n - 1];
+	}
+	
+}
