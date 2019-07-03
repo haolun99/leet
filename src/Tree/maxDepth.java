805a08242143d5246f05cbe3076d9c7094ecb023
Frank Wu <1204926758@qq.com>
2019-07-03 15:06:17 -0400

    add 227 houseRobberThree

--- src/Tree/maxDepth.java
+++ src/Tree/maxDepth.java
@@ -1,5 +1,5 @@
 package Tree;
-
+// 543
 public class maxDepth {
 	public class TreeNode {
 		int val;
@@ -10,16 +10,24 @@ public class maxDepth {
 			val = x;
 		}
 	}
-
-	public int diameterOfBinaryTree(TreeNode root) {
-        return rec_func(root,0);
+	
+	 int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+    	// Wrong if put int max = 0 inside
+        if(root == null)
+            return 0;
+        max = Math.max(max, rec_func(root.left) + rec_func(root.right));
+        diameterOfBinaryTree(root.left); // Iterate left sub-tree recursively
+        diameterOfBinaryTree(root.right);
+        return max;
     }
-    
-	public int rec_func(TreeNode node, int depth){
-        if (node == null)
-            return depth;
-        else
-            return Math.max(rec_func(node.left,depth + 1),rec_func(node.right,depth + 1));}
+    public int rec_func(TreeNode root){
+        if(root == null)  // If there is only one node of the tree, root.left and root.right are null, both return 0
+            return 0;
+        return 1 + Math.max(rec_func(root.left), rec_func(root.right));
+    }
+
+
 
 	
 }
--- src/dfs/HouseRobberThree.java
+++ src/dfs/HouseRobberThree.java
@@ -1,28 +1,61 @@
 package dfs;
+// 337
+import java.util.HashMap;
+import java.util.Map;
 
 public class HouseRobberThree {
 	public class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
-		TreeNode(int x){
+
+		TreeNode(int x) {
 			val = x;
 		}
 	}
-	
-	
+
+	// Solution 1
+	public int robSol(TreeNode root) {
+
+		return helper(root, new HashMap<>());
+	}
+
+	private int helper(TreeNode root, Map<TreeNode, Integer> map) {
+		int val = 0;
+		if (root == null)
+			return 0;
+		if (map.containsKey(root))
+			return map.get(root);
+
+		if (root.left != null) {
+			val += helper(root.left.left, map) + helper(root.left.right, map);
+		}
+
+		if (root.right != null) {
+			val += helper(root.right.left, map) + helper(root.right.right, map);
+		}
+
+		val = Math.max(val + root.val, helper(root.left, map) + helper(root.right, map));
+
+		map.put(root, val);
+
+		return val;
+	}
+
+	// Solution 2
 	public int rob(TreeNode root) {
 		int[] num = dfs(root);
-        return Math.max(num[0], num[1]);
-    }
-	
+		return Math.max(num[0], num[1]);
+	}
+
 	private int[] dfs(TreeNode x) {
-        if (x == null) return new int[2];
-        int[] left = dfs(x.left);
-        int[] right = dfs(x.right);
-        int[] res = new int[2];
-        res[0] = left[1] + right[1] + x.val;
-        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        return res;
-    }
+		if (x == null)
+			return new int[2];// max(0, 0) = 0
+		int[] left = dfs(x.left);
+		int[] right = dfs(x.right);
+		int[] res = new int[2];
+		res[0] = left[1] + right[1] + x.val;
+		res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+		return res;
+	}
 }
