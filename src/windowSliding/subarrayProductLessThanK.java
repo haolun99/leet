49782aeca5e370b702ed097b5501a3843b64e917
Frank Wu <1204926758@qq.com>
2019-07-07 18:50:22 -0400

    latest commit

--- src/windowSliding/subarrayProductLessThanK.java
+++ src/windowSliding/subarrayProductLessThanK.java
@@ -1,4 +1,5 @@
 package windowSliding;
+
 // 713
 public class subarrayProductLessThanK {
 	public int numSubarrayProductLessThanK(int[] nums, int k) {
@@ -16,9 +17,17 @@ public class subarrayProductLessThanK {
 //				count += right - left + 1;
 //			}
 			// while(count >= k) Wrong
-			while(product >= k)
+			while (product >= k)
 				product /= nums[left++];
+
 			count += right - left + 1;
+			/*
+			 * Each step introduces x new subarrays, where x is the size of the current
+			 * window (j + 1 - i); 
+			 * Example: for window (5, 2), when 6 is introduced, (5, (2, (6))) 
+			 * it add 3 new subarray:  
+			 * (6) (2, 6) (5, 2, 6)
+			 */
 		}
 
 		return count;
