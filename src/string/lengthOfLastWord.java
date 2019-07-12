072233af9768daf6ae99d65ce7b9c845c9a8c4b6
Frank Wu <1204926758@qq.com>
2019-07-11 23:40:27 -0400

    add 58 lengthOfLastWord

--- src/string/lengthOfLastWord.java
+++ src/string/lengthOfLastWord.java
@@ -1,7 +1,17 @@
 package string;
-//58
+// 58
 public class lengthOfLastWord {
    public int lengthOfLastWord(String s) {
-        return 0;
+	   int count = 0;
+	   String temp  = s.trim(); // eliminate leading and trailing space
+       for(int i = temp.length() - 1; i >= 0; i--) {
+	   // for(int i = s.length() - 1; i >= 0; i--) { Wrong string s has already change to temp
+	  // for(int i = s.length() - 1; i > 0 ; i--) { Wrong case: [a]
+		   if(temp.charAt(i) != ' ')
+			   count++;
+		   else break;
+	   }
+	   
+	   return count;
     }
 }
