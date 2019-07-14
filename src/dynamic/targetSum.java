1999025ee05980fadc4593919d51f19ca9d037d9
Frank Wu <1204926758@qq.com>
2019-07-13 23:47:08 -0400

    add 494 targetSum

---
+++ src/dynamic/targetSum.java
@@ -0,0 +1,34 @@
+package dynamic;
+// 494
+public class targetSum {
+	// Brute force
+	int result = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        calculate(nums, 0, 0, S);
+        return result;
+    }
+    
+    public void calculate(int[] nums, int i, int sum, int S) {
+    	if(i == nums.length) {
+    		if(sum == S) {
+    			result++;
+    		}
+    	}else {
+    		calculate(nums, i + 1, sum + nums[i], S);
+    		calculate(nums, i + 1, sum - nums[i], S);
+    	}
+    }
+    
+//    public void calculate(int[] nums, int i, int sum, int S){
+//        if(i == nums.length){
+//            if(sum == S){
+//                result++;
+//            }
+//            else{ Wrong: else should point to the first if, not the second if loop
+//                calculate(nums, i + 1, sum + nums[i], S);
+//                calculate(nums, i + 1, sum - nums[i], S);
+//            }
+//        }
+//    }
+	
+}
