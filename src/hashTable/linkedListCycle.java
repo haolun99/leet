e23fc1334c74170bddbc407133cba4502aa3f1bf
Frank Wu <1204926758@qq.com>
2019-07-18 20:59:39 -0400

    add 141 LinkedListCycle

---
+++ src/hashTable/linkedListCycle.java
@@ -0,0 +1,36 @@
+package hashTable;
+// 141
+import java.util.HashSet;
+
+public class linkedListCycle {
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x){
+			val = x;
+			next = null;
+		}
+	}
+	
+	/*
+	 * We go through each node one by one and record each node's reference (or memory address) in a hash table. 
+	 * If the current node is null, we have reached the end of the list and it must not be cyclic. 
+	 * If current node¡¯s reference is in the hash table, then return true.
+	 */
+	public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        
+        while(head != null) {
+        	if(set.contains(head)) {
+        		return true;
+        	}
+        	else {
+        		set.add(head);
+        	}
+        	head = head.next;
+        	
+        }
+        return false;
+    }
+}
