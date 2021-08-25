8066a1bb3653f3e99b130b74b04a5e28fe71a7ab
haolun99 <1204926758@qq.com>
2021-08-25 16:54:38 -0400

    add 36 checkValidSudoku

--- src/Tree/arrayToBST.java
+++ src/Tree/arrayToBST.java
@@ -15,8 +15,36 @@ import javax.swing.tree.TreeNode;
  *     }
  * }
  */
+
 public class arrayToBST {
-    public TreeNode SortedArrayToBST(int[] nums) {
+    // 108
+    public TreeNode sortedArrayToBST(int[] num) {
+
         return null;
     }
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+
+            return null;
+        }
+
+    }
+
 }
