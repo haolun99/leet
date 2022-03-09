aa0df2849405396d3498bcb9789adc2d4188f09e
haolun99 <1204926758@qq.com>
2022-03-08 23:03:44 -0500

    new commit

--- src/array/findWordCanBeFormed.java
+++ src/array/findWordCanBeFormed.java
@@ -2,11 +2,38 @@ package array;
 
 // 1160
 
+import java.util.Arrays;
+
 public class findWordCanBeFormed {
+            public static void main(String[] args){
+                String [] array = {"cat", "bt", "hat", "tree"};
+                String chars = "attach";
+                System.out.println(countCharacters(array, chars));
+            }
 
-        public int countCharacters(String[] words, String chars){
+        public static int countCharacters(String[] words, String chars){
             //
+            int count = 0;
+            int[] seen = new int[26];
+            //Count char of Chars String
+            for (char c : chars.toCharArray())
+                seen[c - 'a']++;
+            // Comparing each word in words
+            for (String word : words) {
+                // simple making copy of seen arr
+                int[] tSeen = Arrays.copyOf(seen, seen.length);
+                int Tcount = 0;
+                for (char c : word.toCharArray()) {
+                    if (tSeen[c - 'a'] > 0) {
+                        tSeen[c - 'a']--;
+                        Tcount++;
+                    }
+                }
+                if (Tcount == word.length())
+                    count += Tcount;
+            }
+            return count;
 
-            return 0;
+
         }
 }
