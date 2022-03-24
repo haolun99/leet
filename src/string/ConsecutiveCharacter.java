aa9a1c2ecb81054f793d9afdc1fccc486a9ba974
haolun99 <1204926758@qq.com>
2022-03-24 17:50:53 -0400

    new commit

--- src/string/ConsecutiveCharacter.java
+++ src/string/ConsecutiveCharacter.java
@@ -2,9 +2,26 @@ package string;
 
 public class ConsecutiveCharacter {
     // 1446
-    public int maxPower(String s) {
+    public static void main(String[] args){
+        String str = "tourist";
+        System.out.println(maxPower(str));
 
+    }
 
-        return 0;
+    public static int maxPower(String s) {
+    char c = s.charAt(0);
+    int maxCount = 1;
+    int count = 1;
+    for(int i = 1; i < s.length(); i++){
+        if(s.charAt(i) == c){
+            count++;
+            maxCount = Math.max(count, maxCount);
+        }else{
+            count = 1;
+            c = s.charAt(i); // iterate to current character, compared with next one later
+        }
+    }
+
+        return maxCount;
     }
 }
