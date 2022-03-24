8e66e855b2754aabd0d6df208e4ff128dc81830b
haolun99 <1204926758@qq.com>
2022-03-24 17:40:12 -0400

    new commit

--- src/math/PalindromeNumber.java
+++ src/math/PalindromeNumber.java
@@ -1,8 +1,31 @@
 package math;
 
 public class PalindromeNumber {
-		public boolean isPalindrome(int x) {
-         
-			return false;
+		//
+	public static void main(String[] args){
+		int num = 121;
+		System.out.println(isPalindrome(num));
+	}
+
+		public static boolean isPalindrome(int x) {
+
+			String temp = Integer.toString(x);
+			boolean flag = false;
+
+			String reversed = new StringBuffer(temp).reverse().toString();
+
+			if(x < 0) return false;
+
+			// if (temp == reversed){
+				 else if (temp.contentEquals(reversed)){
+				flag = true;
+			}
+
+			else{
+				flag = false;
+			}
+
+			return flag;
+
     }
 }
---
+++ src/string/ConsecutiveCharacter.java
@@ -0,0 +1,10 @@
+package string;
+
+public class ConsecutiveCharacter {
+    // 1446
+    public int maxPower(String s) {
+
+
+        return 0;
+    }
+}
