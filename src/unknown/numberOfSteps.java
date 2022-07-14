9de98143902f100e063fa912ea765a2f49f9c45c
haolun99 <1204926758@qq.com>
2022-07-14 14:28:20 -0400

    new commit

---
+++ src/unknown/numberOfSteps.java
@@ -0,0 +1,22 @@
+package unknown;
+
+public class numberOfSteps {
+    // 1342
+
+    public int numberOfSteps(int num){
+        int count = 0;
+
+        while(num > 0){
+            if(num % 2 == 0){
+                num = num / 2;
+                count++;
+            }
+            else{
+                num--;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
