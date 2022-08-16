e53726938d71106d3ca0e23640608c8f26be7119
haolun99 <1204926758@qq.com>
2022-08-16 16:58:09 -0400

    new commit 88

---
+++ src/twoPointer/mergeSortedArray.java
@@ -0,0 +1,23 @@
+package twoPointer;
+
+public class mergeSortedArray {
+
+    // 88
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int pointer1 = m - 1;
+        int pointer2 = n - 1;
+
+        int i = m + n - 1; // scan from the rightmost
+
+        while(pointer2 >= 0){
+            if(pointer1 >= 0 && nums1[pointer1] > nums2[pointer2]){
+                nums1[i--] = nums1[pointer1--];
+            }else{
+                nums1[i--] = nums2[pointer2--];
+            }
+        }
+
+    }
+}
