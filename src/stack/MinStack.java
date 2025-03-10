68a264c4f59d42847e5db217f6cf1f32f0d1bb7e
black <1204926758@qq.com>
2025-03-09 20:08:36 -0400

    add leetcode 155

---
+++ src/stack/MinStack.java
@@ -0,0 +1,50 @@
+package stack;
+
+import java.util.Stack;
+
+// 155
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack .push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if(!stack.isEmpty()) {
+            if(stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
